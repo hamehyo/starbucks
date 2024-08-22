2390edf98897add5f2f95030fca68533d8bf5d3a
hamehyo <33iamhhs33@gmail.com>
2024-08-22 11:01:35 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-
-// header => search
-const searchEl = document.querySelector(".search");
-const searchInputEl = searchEl.querySelector("input");
-searchEl.addEventListener("click", function(){
-    searchInputEl.focus();
-});
-searchInputEl.addEventListener("focus", function(){
-    searchEl.classList.add("focused");
-    searchInputEl.setAttribute("placeholder","통합검색");
-});
-searchInputEl.addEventListener("blur", function(){
-    searchEl.classList.remove("focused");
-    searchInputEl.setAttribute("placeholder","");
-});
-
-
 // right top floating badge & top btn
 const badgeEl = document.querySelector("header .badges");
 const toTopEl = document.querySelector("#to-top");
@@ -145,13 +128,6 @@ spyEls.forEach(function(spyEl){
         .setClassToggle(spyEl,"show")
         .addTo(new ScrollMagic.Controller());
 });
-
-// footer
-
-const thisYear = document.querySelector(".this-year");
-thisYear.textContent = new Date().getFullYear();
-
-
 
 
 
